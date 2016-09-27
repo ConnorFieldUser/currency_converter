fd5570f03d5a01058405438a0a4c6437cc669779
ConnorFieldUser <conquest.field@gmail.com>
2016-09-26 21:15:25 -0400

    after hours of struggle, I have come to the conclusion that I need to stop struggling. Finished the currency converter

--- currency-converter.py
+++ currency-converter.py
@@ -10,21 +10,61 @@ class Money:
         self.symbol = symbol
 
     def convert_to_usd(self):
-            print("usd_version:")
+            # print("usd_version:")
             return self.value / currencies[self.symbol]
             # return currencies(self.value) / Money(self.value)
 
     def __add__(self, other):
         print("added")
-        return self.convert_to_usd()
-        return other.convert_to_usd()
+        added = self.convert_to_usd() + other.convert_to_usd()
+        return added
         # return Money("usd", (self.value + other.value))
-        return Money('usd', self.value + other.value)
+        # return Money('usd', self.value + other.value)
+
+    def __sub__(self, other):
+        print("subtracted")
+        subtracted = self.convert_to_usd() + other.convert_to_usd()
+        return subtracted
+
+    def __mul__(self, other):
+        print("multiplied")
+        multiplied = self.convert_to_usd() * other.convert_to_usd()
+        return multiplied
+
+    def __eq__(self, other):
+        print("is equal:")
+        is_equal = self.convert_to_usd() == other.convert_to_usd()
+        return is_equal
+
+    def __ne__(self, other):
+        print("is not equal:")
+        is_not_equal = self.convert_to_usd() != other.convert_to_usd()
+        return is_not_equal
+
+    def __lt__(self, other):
+        print("is less than:")
+        less_than = self.convert_to_usd() < other.convert_to_usd()
+        return less_than
+
+    def __gt__(self, other):
+        print("is greater than:")
+        greater_than = self.convert_to_usd() > other.convert_to_usd()
+        return greater_than
+
+    def __le__(self, other):
+        print("is less than or equal to:")
+        less_than = self.convert_to_usd() <= other.convert_to_usd()
+        return less_than
+
+    def __ge__(self, other):
+        print("is greater than or equal to:")
+        greater_than = self.convert_to_usd() >= other.convert_to_usd()
+        return greater_than
 
     def __str__(self):
         return "converted value: " + "NOOOOOOO"
 
-    # ge,>= gt,> le,<= lt,< add+ sub- eq,== ne,!= mul*
+# ge,>= gt,> le,<= lt,< add+ sub- eq,== ne,!= mul*
 
 #
 # usd = ("$", 1)
@@ -43,14 +83,25 @@ class Money:
 # Ƀ
 
 
-jpy = Money('jpy', 1)
+jpy = Money('jpy', 100)
 # jpy = jpy.convert_to_usd()
 print(jpy.value)
 
 usd = Money('usd', 1)
 # usd = usd.convert_to_usd()
+
+dd = Money('dd', 999999)
+
+eur = Money('eur', 10)
+
 print(usd.value)
 
 print(jpy + usd)
+
+print(usd - dd)
+
+print(usd * eur)
+
+print(usd == usd)
 
 # print(dd + xbt)
