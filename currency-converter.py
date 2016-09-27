bb3960307d66441695c524b6768c93a011f3aa75
ConnorFieldUser <conquest.field@gmail.com>
2016-09-26 20:22:03 -0400

    minor changes

--- currency-converter.py
+++ currency-converter.py
@@ -16,11 +16,14 @@ class Money:
 
     def __add__(self, other):
         print("added")
+        return self.convert_to_usd()
+        return other.convert_to_usd()
         # return Money("usd", (self.value + other.value))
         return Money('usd', self.value + other.value)
 
-    # def __str__(self):
-    #     return "converted value" + self.value
+    def __str__(self):
+        return "converted value: " + "NOOOOOOO"
+
     # ge,>= gt,> le,<= lt,< add+ sub- eq,== ne,!= mul*
 
 #
@@ -40,15 +43,14 @@ class Money:
 # Ƀ
 
 
-jpy = Money('jpy', 300)
+jpy = Money('jpy', 1)
 # jpy = jpy.convert_to_usd()
-print(jpy)
+print(jpy.value)
 
 usd = Money('usd', 1)
 # usd = usd.convert_to_usd()
-print(usd)
+print(usd.value)
 
-print("added")
 print(jpy + usd)
 
 # print(dd + xbt)
